421b09fcb493a51ec86a82e3e854be95db1ece1c
Tiago Alcantara <tiago.dpalcantara@gmail.com>
2026-05-13 19:23:27 -0300

    feat(api): filter internal fields from receivables and documents responses

--- apps/api/src/modules/documents/documents.service.ts
+++ apps/api/src/modules/documents/documents.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { DocumentsRepository } from './documents.repository';
 import { CreateDocumentDto } from './dto/create-document.dto';
+import { toDocumentResponse } from './dto/document-response.dto';
 
 @Injectable()
 export class DocumentsService {
   constructor(private readonly repo: DocumentsRepository) {}
 
   async create(data: CreateDocumentDto) {
-    return this.repo.create(data);
+    return toDocumentResponse(await this.repo.create(data));
   }
 
   async findByReceivable(receivableId: string) {
-    return this.repo.findByReceivable(receivableId);
+    return (await this.repo.findByReceivable(receivableId)).map(toDocumentResponse);
   }
 }
